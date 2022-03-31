1dc69524a5eeb5cdb531b54d3721544f405184eb
Janghwan Kim(Harley) <68185569+harley-hwan@users.noreply.github.com>
2022-03-31 18:16:11 +0900

    Update C#Project

--- repos/Part 2. 자료구조와 알고리즘/Algorithm/Maze/Board.cs
+++ repos/Part 2. 자료구조와 알고리즘/Algorithm/Maze/Board.cs
@@ -12,6 +12,9 @@ namespace Maze
         public TileType[,] Tile { get; private set; }    //배열
         public int Size { get; private set; }
 
+        public int DestY { get; private set; }
+        public int DestX { get; private set; }
+
         Player _player;
 
         public enum TileType
@@ -28,6 +31,9 @@ namespace Maze
 
             Tile = new TileType[size, size];
             Size = size;
+
+            DestY = Size - 2;
+            DestX = Size - 2;
 
             // Mazes for Programmers
             //GenerateByBinaryTree();
@@ -150,6 +156,8 @@ namespace Maze
                     // 플레이어 좌표를 갖고 와서, 그 좌표랑 현재 y, x가 일치하면 플레이어 전용 색상으로 표시
                     if (y == _player.PosY && x == _player.PosX)
                         Console.ForegroundColor = ConsoleColor.Blue;
+                    else if (y == DestY && x == DestX)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                     else
                         Console.ForegroundColor = GetTileColor(Tile[y, x]);
                     
--- repos/Part 2. 자료구조와 알고리즘/Algorithm/Maze/Player.cs
+++ repos/Part 2. 자료구조와 알고리즘/Algorithm/Maze/Player.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace Maze
 {
+    class Pos
+    {
+        public Pos(int y, int x) { Y = y; X = x; }
+        public int Y;
+        public int X;
+    }
     class Player
     {
         public int PosX { get; private set; }   // Player 좌표는 Player만 고칠 수 있다.
@@ -14,49 +20,78 @@ namespace Maze
         Random _random = new Random();
         Board _board;
 
-        public void Initialize(int posY, int posX, int destY, int destX, Board board)
+        enum Dir
+        {
+            Up = 0,
+            Left = 1,
+            Down = 2,
+            Right = 3
+        }
+
+        int _dir = (int)Dir.Up;
+        List<Pos> _points = new List<Pos>();
+
+        public void Initialize(int posY, int posX, Board board)
         {
             PosX = posX;
             PosY = posY;
             _board = board;
+
+            // 현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다.
+            int[] frontY = new int[] { -1, 0, 1, 0 };   // Up, Left, Down, Right
+            int[] frontX = new int[] { 0, -1, 0, 1 };
+
+            int[] rightY = new int[] { 0, -1, 0, 1 };   // Up, Left, Down, Right
+            int[] rightX = new int[] { 1, 0, -1, 0 };
+
+            _points.Add(new Pos(PosY, PosX));
+
+            // 목적지 도착하기 전에는 계속 진행
+            while (PosY != board.DestY || PosX != board.DestX)
+            {
+                // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
+                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
+                {
+                    // 오른쪽 방향으로 90도 회전 후
+                    _dir = (_dir - 1 + 4) % 4;
+                    // 앞으로 한 보 전진
+                    PosY += frontY[_dir];
+                    PosX += frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                // 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+                {
+                    // 앞으로 한 보 전진
+                    PosY += frontY[_dir];
+                    PosX += frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                else
+                {
+                    // 왼쪽 방향으로 90도 회전   (왼쪽 두번 회전하면 후진이 됨)
+                    _dir = (_dir + 1 + 4) % 4;
+                }
+            }
         }
 
         const int MOVE_TICK = 10;
         int _sumTick = 0;
+        int _lastIndex = 0;
 
         public void Update(int deltaTick)
         {
+            if (_lastIndex >= _points.Count)
+                return;
+
             _sumTick += deltaTick;
             if (_sumTick >= MOVE_TICK)
             {
                 _sumTick = 0;
 
-                //여기에다가 0.1초마다 실횡될 로직을 넣어준다.
-                int randValue = _random.Next(0, 4);
-                switch (randValue)
-                {
-                    case 0:     // 상
-                        if (PosY - 1 >= 0 && _board.Tile[PosY - 1, PosX] == Board.TileType.Empty)
-                            PosY--;
-                        break;
-                              
-                    case 1:     // 하
-                        if (PosY + 1 < _board.Size && _board.Tile[PosY + 1, PosX] == Board.TileType.Empty)
-                            PosY++;
-                        break;
-
-                    case 2:     // 좌
-                        if (PosX - 1 >= 0 && _board.Tile[PosY, PosX - 1] == Board.TileType.Empty)
-                            PosX--;
-                        break; 
-
-                    case 3:     // 우
-                        if (PosX + 1 < _board.Size && _board.Tile[PosY, PosX + 1] == Board.TileType.Empty)
-                            PosX++;
-                        break;
-                       
-
-                }
+                PosY = _points[_lastIndex].Y;
+                PosX = _points[_lastIndex].X;
+                _lastIndex++;
             }
         }
     }
